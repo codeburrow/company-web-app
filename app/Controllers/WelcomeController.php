b136ecf91db2cd9369c238ccfe6a51f77d2d5246
Antony <anthonykalogeropoulos@gmail.com>
2016-02-22 01:32:05 +0200

    Make email (form submission) functional

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -45,22 +45,38 @@ class WelcomeController extends Controller
 	}
 
 	public function formPost()
-    {
-        $title = 'Code Burrow';
+	{
+		$title = 'Code Burrow';
 
-        $users = $this->userRepository->getAll();
+		$users = $this->userRepository->getAll();
 
-        shuffle($users);
+		shuffle($users);
 
-        $randomQuote = $this->quotesRepository->getRandom();
+		$randomQuote = $this->quotesRepository->getRandom();
 
-	    var_dump($_POST);
+		if ( isset($_POST) ) {
+			$to = "anthonykalogeropoulos@gmail.com"; // this is your Email address
+			$from = $_POST['inputEmail3']; // this is the sender's Email address
+			$first_name = $_POST['name'];
+			$subject = "Message From CodeBurrow";
+			$subject2 = "Copy of Your Message to CodeBurrow.com";
+			$message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
+			$message2 = $first_name . ", here is a copy of your message: " . "\n\n" . $_POST['message'];
 
-	    print_r($_POST['name']);
+			$headers = "From:" . $from;
+			$headers2 = "From:" . $to;
+			mail($to, $subject, $message, $headers);
+			mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
+			echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+			// You can also use header('Location: thank_you.php'); to redirect to another page.
+			// You cannot use header and echo together. It's one or the other.
+		} else {
+			echo "Form submission unsuccessful";
+		}
 
-	    echo "Hello bbq";
+//		echo "Hello bbq";
 //        return $this->views->render('welcome', compact('users', 'title', 'randomQuote'));
-    }
+	}
 
 	public function getParameters()
 	{
