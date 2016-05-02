f694255ed568a6b0b9cb00dfbc0093c76fa7c484
Antony <anthonykalogeropoulos@gmail.com>
2016-05-02 15:12:24 +0300

    Attempt 1 to use SwiftMailer

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -1,7 +1,10 @@
 <?php namespace Burrow\Controllers;
 
+use Swift_Mailer;
+use Swift_Message;
 use Burrow\Repositories\QuoteRepositories\StaticQuoteRepository;
 use Burrow\Repositories\UserRepositories\StaticUserRepository;
+use Swift_SmtpTransport;
 
 /**
  * @author Antony Kalogeropoulos
@@ -46,36 +49,80 @@ class WelcomeController extends Controller
 
 	public function formPost()
 	{
-		$title = 'Code Burrow';
+//		$title = 'Code Burrow';
+//
+//		$users = $this->userRepository->getAll();
+//
+//		shuffle($users);
+//
+//		$randomQuote = $this->quotesRepository->getRandom();
+//
+//		if ( isset($_POST) ) {
+//			$to = "support@codeburrow.com"; // this is your Email address
+//			$from = $_POST['email']; // this is the sender's Email address
+//			$name = $_POST['name'];
+//			$subject = "Message From CodeBurrow:" . $_POST['subject'];
+//			$subject2 = "Copy of Your Message to CodeBurrow.com: " . $_POST['subject'];
+//			$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+//			$message2 = $name . ", here is a copy of your message: " . "\n\n" . $_POST['message'];
+//
+//			$headers = "From:" . $from;
+//			$headers2 = "From:" . $to;
+//			mail($to, $subject, $message, $headers);
+//			mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
+//			echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+//			// You can also use header('Location: thank_you.php'); to redirect to another page.
+//			// You cannot use header and echo together. It's one or the other.
+//		} else {
+//			echo "Form submission unsuccessful";
+//		}
 
-		$users = $this->userRepository->getAll();
+		// Create the Transport
+		$transport = Swift_SmtpTransport::newInstance('smtp.secureserver.net.', 25)
+//			->setUsername('your username')
+//			->setPassword('your password')
+		;
 
-		shuffle($users);
+		/*
+		You could alternatively use a different transport such as Sendmail or Mail:
 
-		$randomQuote = $this->quotesRepository->getRandom();
+		// Sendmail
+		$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
 
-		if ( isset($_POST) ) {
-			$to = "support@codeburrow.com"; // this is your Email address
-			$from = $_POST['email']; // this is the sender's Email address
-			$name = $_POST['name'];
-			$subject = "Message From CodeBurrow:" . $_POST['subject'];
-			$subject2 = "Copy of Your Message to CodeBurrow.com: " . $_POST['subject'];
-			$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-			$message2 = $name . ", here is a copy of your message: " . "\n\n" . $_POST['message'];
+		// Mail
+		$transport = Swift_MailTransport::newInstance();
+		*/
 
-			$headers = "From:" . $from;
-			$headers2 = "From:" . $to;
-			mail($to, $subject, $message, $headers);
-			mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
-			echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-			// You can also use header('Location: thank_you.php'); to redirect to another page.
-			// You cannot use header and echo together. It's one or the other.
+		// Create the Mailer using your created Transport
+		$mailer = Swift_Mailer::newInstance($transport);
+
+		// Create the message
+		$message = Swift_Message::newInstance()
+			// Give the message a subject
+			->setSubject('Email From CodeBurrow.com')
+			// Set the From address with an associative array
+			->setFrom(array('a.kalogeropoulos@acg.edu' => 'Antony Kalogeropoulos'))
+			// Set the To addresses with an associative array
+			->setTo(array('antony@codeburrow.com', 'anthonykalogeropoulos@gmail.com' => 'Antony'))
+			// Give it a body
+			->setBody('Test Test Test');
+
+		// Optionally add any attachments
+//			->attach(Swift_Attachment::fromPath('my-document.pdf'))
+
+		// Send the message
+		/**
+		 * @var \Swift_Mime_Message $message
+		 */
+		$result = $mailer->send($message);;
+
+		//Confirm Email Sent
+		if ($result > 0) {
+			echo "Mail Sent. Thank you ";
 		} else {
 			echo "Form submission unsuccessful";
 		}
 
-//		echo "Hello bbq";
-//        return $this->views->render('welcome', compact('users', 'title', 'randomQuote'));
 	}
 
 	public function getParameters()
@@ -92,6 +139,6 @@ class WelcomeController extends Controller
 	{
 		$title = 'Blog';
 
-		return $this->views->render( 'blog', compact('title') );
+		return $this->views->render('blog', compact('title'));
 	}
 }
