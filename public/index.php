226c53883188b674fc47075fc751e3c7a4add20a
Antony <anthonykalogeropoulos@gmail.com>
2016-05-02 22:48:27 +0300

    Remove contact form from master template because it seems there is a problem - maybe template loads slower or does not recognize native php

--- public/index.php
+++ public/index.php
@@ -28,7 +28,7 @@ require __DIR__ . '/../bootstrap/setUp.php';
 $mux = new \Pux\Mux;
 
 $mux->get('/', ['Burrow\Controllers\WelcomeController', 'index']);
-$mux->get('/getGlobals', ['Burrow\Controllers\WelcomeController', 'getGlobals']);
+//$mux->get('/getGlobals', ['Burrow\Controllers\WelcomeController', 'getGlobals']);
 $mux->get('/index.php', ['Burrow\Controllers\WelcomeController', 'index']);
 $mux->get('/blog', ['Burrow\Controllers\WelcomeController', 'showBlog']);
 $mux->post('/', ['Burrow\Controllers\WelcomeController', 'formPost']);
