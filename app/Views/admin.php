18f533939d5ad743f2eb6e04b09f6151a1608659
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-06-20 02:44:40 +0300

    BugFix3: Display posts nicely in admin page

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -38,17 +38,20 @@ Dashboard
                 ================================================== -->
                 <div class="col-sm-12">
                     <div class="section-title">
-                        <form action="/admin.php" method="post">
-                            <label for="author">Author: </label>
-                            <input type="text" name="author" id="author" value="<?php echo $post['author']; ?>">
-
-                            <label for="title">Title: </label>
-                            <input type="text" name="title" id="title" value="<?php echo $post['title']; ?>">
+                        <form role="form" action="/admin.php" method="post">
+                            <div class="form-group">
+                                <label for="author">Author: </label>
+                                <input style="width: 90px; height: 390px;" type="text" name="author" id="author" value="<?php echo $post['author']; ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="title">Title: </label>
+                                <input style="width: 90px; height: 390px;" type="text" name="title" id="title" value="<?php echo $post['title']; ?>">
+                            </div>
                             <hr>
-
-                            <label for="content">Content: </label>
-                            <input type="text" name="content" id="content" value="<?php echo $post['content']; ?>">
-
+                            <div class="form-group">
+                                <label for="content">Content: </label>
+                                <input style="width: 90px; height: 390px;" type="text" name="content" id="content" value="<?php echo $post['content']; ?>">
+                            </div>
                             <input type="submit" class="btn btn-success">
                         </form>
                     </div>
