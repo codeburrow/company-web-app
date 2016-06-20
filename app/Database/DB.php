b6878731b55f8a443b352fa1728369e1e116ac47
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-06-20 03:34:01 +0300

    Update post from admin panel

--- app/Database/DB.php
+++ app/Database/DB.php
@@ -65,8 +65,8 @@ class DB
         $content = $data['content'];
 
         $stmt = $this->conn->prepare("UPDATE Test.posts
-SET Test.posts.author=$author, Test.posts.title=$title, Test.posts.content=$content
-WHERE Test.posts.id=$id;");
+SET Test.posts.author='$author', Test.posts.title='$title', Test.posts.content='$content'
+WHERE Test.posts.id='$id';");
         $stmt->execute();
 
         // set the resulting array to associative
