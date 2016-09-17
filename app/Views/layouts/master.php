f6a4c78fab038836f9e0b147793372136292ebf8
Rizart Dokollari <r.dokollari@gmail.com>
2016-09-17 04:36:35 +0000

    Concatante scripts, some css, and add environment variables.

--- app/Views/layouts/master.php
+++ app/Views/layouts/master.php
@@ -2,68 +2,41 @@
 <html lang="en">
 <head>
 
-	<meta charset="utf-8">
+    <meta charset="utf-8">
 
-	<!-- Template site title
+    <!-- Template site title
    ================================================== -->
-	<title><?= $this->section('title'); ?></title>
+    <title><?= $this->section('title'); ?></title>
 
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="keywords" content="codeburrow, web, software, development, design">
-	<meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="codeburrow, web, software, development, design">
+    <meta name="description" content="">
 
-	<link rel="shortcut icon" href="img/logo_trans.png">
-	<!--
+    <link rel="shortcut icon" href="img/logo_trans.png">
+    <!--
 
-	Template 2078 Adventure
+    Template 2078 Adventure
 
-	http://www.tooplate.com/view/2078-adventure
+    http://www.tooplate.com/view/2078-adventure
 
-	-->
-	<!-- Bootstrap CSS
+    -->
+    <!-- Bootstrap CSS
    ================================================== -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="build/above-the-fold.min.css">
 
-	<!-- Animate CSS
-   ================================================== -->
-	<link rel="stylesheet" href="css/animate.min.css">
+<!--    <link rel="stylesheet" href="css/bootstrap.min.css">-->
+<!--    <link rel="stylesheet" href="css/animate.min.css">-->
+<!--    <link rel="stylesheet" href="css/font-awesome.min.css">-->
+<!--    <link rel="stylesheet" href="css/et-line-font.css">-->
+    <link rel="stylesheet" href="css/nivo-lightbox.css">
+    <link rel="stylesheet" href="css/nivo_themes/default/default.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/bxslider.css">
+    <link rel="stylesheet" href="css/style.css">
 
-	<!-- Font Icons
-   ================================================== -->
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/et-line-font.css">
-
-	<!-- Nivo Lightbox CSS
-   ================================================== -->
-	<link rel="stylesheet" href="css/nivo-lightbox.css">
-	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
-
-	<!-- Owl Carousel CSS
-   ================================================== -->
-	<link rel="stylesheet" href="css/owl.theme.css">
-	<link rel="stylesheet" href="css/owl.carousel.css">
-
-	<!-- BxSlider CSS
-   ================================================== -->
-	<link rel="stylesheet" href="css/bxslider.css">
-
-	<!-- Main CSS
-	================================================== -->
-	<link rel="stylesheet" href="css/style.css">
-
-	<!-- Google web font
-   ================================================== -->
-	<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
-
-	<script src='https://cdn.tinymce.com/4/tinymce.min.js'></script>
-	<script>
-		tinymce.init({
-			selector: '.mytextarea',
-			plugins : 'advlist autolink link image lists charmap print preview'
-		});
-	</script>
-
+    <link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
 </head>
 
 <body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
@@ -73,50 +46,48 @@
 ================================================== -->
 <section class="preloader">
 
-	<div class="sk-rotating-plane"></div>
+    <div class="sk-rotating-plane"></div>
 
 </section>
 
 
 <!-- Content section
 ================================================== -->
-<?=$this->section('content')?>
+<?= $this->section('content') ?>
 
 
 <!-- Footer section
 ================================================== -->
 <footer>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 col-sm-12">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
 
-				<a style="color: #ffa400;" href="http://www.codeburrow.com"><h2 class="wow bounceIn">CodeBurrow</h2></a>
-				<ul class="social-icon">
-					<li><a href="https://www.linkedin.com/company/codeburrow" class="fa fa-linkedin wow fadeIn" data-wow-delay="0.3s"></a></li>
-					<li><a href="https://github.com/codeburrow" class="fa fa-github wow fadeIn" data-wow-delay="1.3s"></a></li>
-				</ul>
-				<p>Copyright &copy; 2016 CodeBurrow
+                <a style="color: #ffa400;" href="http://www.codeburrow.com"><h2 class="wow bounceIn">CodeBurrow</h2></a>
+                <ul class="social-icon">
+                    <li><a href="https://www.linkedin.com/company/codeburrow" class="fa fa-linkedin wow fadeIn"
+                           data-wow-delay="0.3s"></a></li>
+                    <li><a href="https://github.com/codeburrow" class="fa fa-github wow fadeIn"
+                           data-wow-delay="1.3s"></a></li>
+                </ul>
+                <p>Copyright &copy; 2016 CodeBurrow
 
-					| Back To: <a rel="nofollow" href="#home" target="_parent">Top</a></p>
+                    | Back To: <a rel="nofollow" href="#home" target="_parent">Top</a></p>
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </footer>
 
-
-<!-- Javascript
-================================================== -->
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/smoothscroll.js"></script>
-<script src="js/nivo-lightbox.min.js"></script>
-<script src="js/jquery.easing-1.3.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.parallax.js"></script>
-<script src="js/wow.min.js"></script>
-<script src="js/custom.js"></script>
+<script src="build/master.min.js" async onload="initTinymce"></script>
+<script>
+    function initTinymce() {
+        tinymce.init({
+            selector: '.mytextarea',
+            plugins: 'advlist autolink link image lists charmap print preview'
+        });
+    }
+</script>
 
 </body>
 </html>
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,56 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gulpConcat = require('gulp-concat'),
+    gulpRename = require('gulp-rename'),
+    gulpUglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css');
+
+
+var buildDirectory = './public/build/';
 
 gulp.task('default', function () {
+    return gulp.src([
+            './public/js/tinymce.min.js',
+            './public/js/jquery.js',
+            './public/js/bootstrap.min.js',
+            './public/js/smoothscroll.js',
+            './public/js/nivo-lightbox.min.js',
+            './public/js/jquery.easing-1.3.js',
+            './public/js/plugins.js',
+            './public/js/owl.carousel.min.js',
+            './public/js/jquery.parallax.js',
+            './public/js/wow.min.js',
+            './public/js/custom.js'
+        ])
+        .pipe(gulpConcat('concat.js'))
+        .pipe(gulpRename('master.min.js'))
+        .pipe(gulp.dest(buildDirectory))
+        .pipe(gulpUglify())
+        .pipe(gulp.dest(buildDirectory));
+});
+
+gulp.task('css', function () {
+    // return gulp.src([
+    //         './public/css/bootstrap.min.css'
+    //     ])
+    //     .pipe(gulpConcat('concat.css'))
+    //     .pipe(gulp.dest(buildDirectory))
+    //     .pipe(gulpRename('master.min.css'))
+    //     .pipe(gulpUglify())
+    //     .pipe(gulp.dest(buildDirectory));
+
+    return gulp.src([
+            './public/css/bootstrap.min.css',
+            './public/css/animate.min.css',
+            // './public/css/bxslider.css',
+            // './public/css/style.css'
+            './public/css/font-awesome.min.css',
+            './public/css/et-line-font.css',
+            // './public/css/nivo-lightbox.css',
+            // './public/css/nivo_themes/default/default.css',
+            // './public/css/owl.theme.css',
+            // './public/css/owl.carousel.css',
+        ])
+        .pipe(gulpConcat('above-the-fold.min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest(buildDirectory));
 });
